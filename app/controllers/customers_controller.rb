fcc04313c2ea24065bf9fb8e72d5f48752978e33
James Yeates <jimibue@gmail.com>
2020-09-02 13:00:46 -0700

    half of files committed

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,46 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+
   def index
+    # @customers = current_user.customers
+    @customers = Customer.all_customers(current_user.id)
   end
 
   def show
   end
 
   def new
+    @customer = Customer.new
+  end
+
+  def create
+    # current_user.customers.create(customer_params)
+    Customer.create_customer(customer_params, current_user.id)
+    redirect_to customers_path
   end
 
   def edit
+  end
+
+  def update
+    @customer.update(customer_params)
+    redirect_to customers_path
+  end
+
+  def destroy
+    # @customer.destroy
+    Customer.destroy_customer(@customer.id)
+    redirect_to customers_path
+  end
+
+  private
+
+  def set_customer
+    # @customer = current_user.customers.find(params[:id])
+    @customer = Customer.single_customer(current_user.id, params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :phone)
   end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,45 @@
 class Customer < ApplicationRecord
   belongs_to :user
+
+  def self.all_customers(user_id)
+    Customer.find_by_sql("
+      SELECT * FROM customers as c
+      where c.user_id = #{user_id};
+      ")
+  end
+
+  def self.single_customer(user_id, customer_id)
+    Customer.find_by_sql(
+      ["SELECT * FROM customers as c where c.user_id = ? AND c.id = ?",
+       user_id,
+       customer_id]
+    ).first
+  end
+
+  def self.create_customer(customer, id)
+    Customer.find_by_sql(
+      ["INSERT INTO customers (first_name, last_name, email, phone, user_id, created_at, updated_at )
+      Values (:first, :last, :email, :phone, :user_id, :created_at, :updated_at)
+    ", {
+        first: customer[:first_name],
+        last: customer[:last_name],
+        email: customer[:email],
+        phone: customer[:phone],
+        user_id: id,
+        created_at: DateTime.now,
+        updated_at: DateTime.now,
+      }]
+    )
+  end
+
+  def self.destroy_customer(customer_id)
+    Customer.find_by_sql("
+      DELETE FROM customers as c
+      WHERE c.id = #{customer_id}
+      ")
+  end
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
